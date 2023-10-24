1a5f7ca27c0bbdfc47008dc4e8bf904f51da8ac9
Микита Кузьмінов <kuzminov.mykyta26@icloud.com>
2023-10-24 10:58:08 +0300

    Shape

---
+++ SwiftUI-Bootcamp-1/SwiftUI-Bootcamp-1/ShapeBootcamp.swift
@@ -0,0 +1,51 @@
+//
+//  ShapeBootcamp.swift
+//  SwiftUI-Bootcamp-1
+//
+//  Created by Микита Кузьмінов on 24.10.2023.
+//
+
+import SwiftUI
+
+struct ShapeBootcamp: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            // Створює коло
+            Circle()
+            // Змінює колір фігури
+                .fill(.blue)
+                .frame(width: 100, height: 100)
+            
+            // Створює еліпс
+            Ellipse()
+            // Вирізає, залишаючи лише контур
+                .stroke(.red)
+                .frame(width: 150, height: 100)
+            
+            // Створює капсулу
+            Capsule()
+            // Обрізає фігуру
+                .trim(from: 0.3, to: 1.0)
+                .frame(width: 150, height: 100)
+            
+            // Створює прямокутник з тупими кутами
+            Rectangle()
+            // Вирізає, залишаючи лише контур з розміром, який ви обрали
+                .stroke(lineWidth: 5)
+                .frame(width: 100, height: 100)
+            
+            // Створює прямокутник з круглими кутами
+            RoundedRectangle(cornerRadius: 40)
+            // Вирізає, залишаючи лише контур та змінює його стиль
+                .stroke(.green, style:
+                            StrokeStyle(lineWidth: 20,
+                                        lineCap: .butt,
+                                        dash: [10]))
+                .frame(width: 100, height: 100)
+        }
+    }
+}
+
+#Preview {
+    ShapeBootcamp()
+}
